9da74a0217981f6ba484eb71dadfa7e0d2d85e7e
Ashok Modhave <ashok.modhave143@gmail.com>
2024-01-19 09:39:28 +1300

    setting up husky

--- apps/codeheadnz-ltd/app/_common/components/AppRoot/AppRoot.spec.tsx
+++ apps/codeheadnz-ltd/app/_common/components/AppRoot/AppRoot.spec.tsx
@@ -3,10 +3,26 @@ import { render } from '@testing-library/react';
 import AppRoot from './AppRoot';
 import { AppConfigState } from '@common/redux/appConfigSlice';
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: any) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  }),
+});
+
 describe('AppRoot', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <AppRoot env={{} as AppConfigState['environmentVariables']} />
+      <AppRoot env={{} as AppConfigState['environmentVariables']}>
+        <div>AppRoot</div>
+      </AppRoot>
     );
     expect(baseElement).toBeTruthy();
   });

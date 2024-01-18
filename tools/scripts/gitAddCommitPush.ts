fc7257712f5d4f4a579c59f091a3c6857c6c31ee
Ashok Modhave <ashok.modhave143@gmail.com>
2024-01-19 09:06:10 +1300

    Add git add, commit, push as a scrit

--- tools/scripts/gitAddCommitPush.ts
+++ tools/scripts/gitAddCommitPush.ts
@@ -1,6 +1,12 @@
 const execSync = require('child_process').execSync;
-const msg = process.argv[1];
-// execSync('git add . && git commit -m "' + msg + '" && git push', {
-//   stdio: [0, 1, 2],
-// });
-console.log(process.argv);
+const msg = process.argv[2];
+
+if (!msg) {
+  console.log('No commit message provided');
+  process.exit(1);
+}
+execSync('git add . && git commit -m "' + msg + '"', {
+  stdio: [0, 1, 2],
+});
+execSync('git push');
+console.log('Done!');

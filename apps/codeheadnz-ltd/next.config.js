07456f67b87cc1a96ab0650cc89723a23a40ef0d
Ashok Modhave <ashok.modhave143@gmail.com>
2025-04-20 01:27:24 +1200

    update webpack config

--- apps/codeheadnz-ltd/next.config.js
+++ apps/codeheadnz-ltd/next.config.js
@@ -2,6 +2,7 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const path = require('path');
 
 const BASE_PATH = process.env.BASE_PATH || '';
 
@@ -28,6 +29,14 @@ const nextConfig = {
   },
   images: {
     domains: ['images.pexels.com'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    };
+    return config;
   },
 };
 
